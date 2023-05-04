149bc8052d19daaf2a0482da3ab20df8cdac4ca7
kaleb <kalebclementino@gmail.com>
2023-05-04 15:48:51 -0300

    Adicionando dados fakes

--- contact/models.py
+++ contact/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -19,6 +24,9 @@ class Contact(models.Model):
     picture = models.ImageField(blank=True,upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
         Category,on_delete=models.SET_NULL,blank=True,null=True
+    )
+    owner = models.ForeignKey(
+        User,on_delete=models.SET_NULL,blank=True,null=True
     )
 
     def __str__(self) -> str:
